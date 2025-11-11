ef54337c1060c413fa1f64d90fcdd2db30fe2832
Vanshit Patel <vanshitpatel10@gmail.com>
2025-11-11 11:24:16 +0530

    backend live in mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/forge_erp', {
+mongoose.connect(process.env.MONGODB_LIVE || 'mongodb://localhost:27017/forge_erp', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
